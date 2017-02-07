861c6bb94d0f12ebc33da9829798b10239e3a973
ChildOfGodLiuqi <zxcvbnm19921108@gmail.com>
2017-02-07 15:22:56 +0800

    Update AuthComparator.java

--- src/main/java/rml/comparator/AuthComparator.java
+++ src/main/java/rml/comparator/AuthComparator.java
@@ -8,6 +8,7 @@ public class AuthComparator implements Comparator<Tauth> {
 
 	public int compare(Tauth o1, Tauth o2) {
 		int xiugai = 166;
+		int merger = 999;		
 		int i1 = o1.getCseq() != null ? o1.getCseq().intValue() : -1;
 		int i2 = o2.getCseq() != null ? o2.getCseq().intValue() : -1;
 		return i1 - i2;
