d2586eec82a13dd907c4088b6f05da889b0a018e
wt_home <zxcvbnm19921108@gmail.com>
2017-02-07 15:05:52 +0800

    testtest

--- src/main/java/rml/comparator/AuthComparator.java
+++ src/main/java/rml/comparator/AuthComparator.java
@@ -7,7 +7,7 @@ import rml.model.po.Tauth;
 public class AuthComparator implements Comparator<Tauth> {
 
 	public int compare(Tauth o1, Tauth o2) {
-		int xiugai = 1;
+		int xiugai = 166;
 		int i1 = o1.getCseq() != null ? o1.getCseq().intValue() : -1;
 		int i2 = o2.getCseq() != null ? o2.getCseq().intValue() : -1;
 		return i1 - i2;
